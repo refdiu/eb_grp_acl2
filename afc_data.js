718ce13608fe9c425d20b80cc78bfa5b8d2f0ea7
refdiu <lahirishraman@gmail.com>
2026-08-21 19:40:28 +0530

    started creating a script that accesses and manipulates table elements

---
+++ afc_data.js
@@ -0,0 +1,14 @@
+//let 'em know
+var eb_mp = document.getElementById("ebgrp").rows[1].cells[1];
+var eb_w = document.getElementById("ebgrp").rows[1].cells[2];
+var eb_d = document.getElementById("ebgrp").rows[1].cells[3];
+var eb_l = document.getElementById("ebgrp").rows[1].cells[4];
+var eb_gf = document.getElementById("ebgrp").rows[1].cells[5];
+var eb_ga = document.getElementById("ebgrp").rows[1].cells[6];
+var eb_gd = document.getElementById("ebgrp").rows[1].cells[7];
+var eb_pts = document.getElementById("ebgrp").rows[1].cells[8];
+
+var eb = document.getElementById("ebgrp").rows[1].cells[0];
+
+var eb_played = 6;
+eb_mp.innerHTML = eb_played;
